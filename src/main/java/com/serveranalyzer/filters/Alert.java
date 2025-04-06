8c27b77be10f60283044c7ff078f86726ce5f641
sharanmadhav <sharanmadhav07@gmail.com>
2025-04-06 19:14:14 +0530

    new beans added

---
+++ src/main/java/com/serveranalyzer/filters/Alert.java
@@ -0,0 +1,32 @@
+package com.serveranalyzer.filters;
+
+public class Alert {
+
+    private String timeStamp;
+    private String message;
+    private String level;
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
---
+++ src/main/java/com/serveranalyzer/filters/BuildUpdateInfo.java
@@ -0,0 +1,14 @@
+package com.serveranalyzer.filters;
+
+public class BuildUpdateInfo {
+
+    private String timeStamp;
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
